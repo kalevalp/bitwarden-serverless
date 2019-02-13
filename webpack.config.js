e4fd565e3490f3a6b629c7f2ebba159d2f8052c8
Kalev Alpernas <kalev.alp@gmail.com>
2019-02-13 14:59:22 +0200

    Updated packages + fixed some error in webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,20 @@
 const path = require('path');
 // eslint-disable-next-line import/no-unresolved
 const slsw = require('serverless-webpack');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
   entry: slsw.lib.entries,
   target: 'node',
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['babel-loader'],
-      include: __dirname,
-      exclude: /node_modules/,
-    }],
-  },
-  externals: ['aws-sdk'],
+  // module: {
+  //   loaders: [{
+  //     test: /\.js$/,
+  //     loaders: ['babel-loader'],
+  //     include: __dirname,
+  //     exclude: /node_modules/,
+  //   }],
+  // },
+  externals: [nodeExternals()],
   output: {
     libraryTarget: 'commonjs',
     path: path.join(__dirname, '.webpack'),
