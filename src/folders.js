dec48420821f932dd8c29f6b3f4886333bb53872
Kalev Alpernas <kalev.alp@gmail.com>
2019-02-18 15:00:01 +0200

    Removed webpack copy plugin, added original file to webpack entries, fixed file path of original file in wrapper.

--- src/folders.js
+++ src/folders.js
@@ -1,6 +1,6 @@
 const recorder = require('watchtower-recorder');
 const aws      = require('aws-sdk');
-const dummy    = require('./folders-original');
+//const dummy    = require('./folders-original');
 
 
 // const s3  = new aws.DynamoDb();
@@ -26,6 +26,6 @@ const mock = {
         }
 })};
 
-module.exports.postHandler = recorder.createRecordingHandler('folders-original.js', 'postHandler' , mock);
-module.exports.putHandler = recorder.createRecordingHandler('folders-original.js', 'putHandler' , mock);
-module.exports.deleteHandler = recorder.createRecordingHandler('folders-original.js', 'deleteHandler' , mock);
+module.exports.postHandler = recorder.createRecordingHandler('src/folders-original.js', 'postHandler' , mock);
+module.exports.putHandler = recorder.createRecordingHandler('src/folders-original.js', 'putHandler' , mock);
+module.exports.deleteHandler = recorder.createRecordingHandler('src/folders-original.js', 'deleteHandler' , mock);
--- webpack.config.js
+++ webpack.config.js
@@ -2,32 +2,26 @@ const path = require('path');
 // eslint-disable-next-line import/no-unresolved
 const slsw = require('serverless-webpack');
 const nodeExternals = require('webpack-node-externals');
-const CopyWebpackPlugin = require('copy-webpack-plugin')
 
-module.exports = {
-  entry: slsw.lib.entries,
-  target: 'node',
-  // module: {
-  //   loaders: [{
-  //     test: /\.js$/,
-  //     loaders: ['babel-loader'],
-  //     include: __dirname,
-  //     exclude: /node_modules/,
-  //   }],
-  // },
-  externals: [nodeExternals()],
-  output: {
-    libraryTarget: 'commonjs',
-    path: path.join(__dirname, '.webpack'),
-    filename: '[name].js',
-  },
-    plugins: [
-	new CopyWebpackPlugin([
-	    {
-		from: 'src/',
-		toType: 'file',
-		test: /original.png$/,
-	    },
-	], {})
-    ]
+slsw.lib.entries['src/folders-original'] = './src/folders-original.js'
+
+module.exports = {   
+    
+    entry: slsw.lib.entries,
+    
+    target: 'node',
+    // module: {
+    //   loaders: [{
+    //     test: /\.js$/,
+    //     loaders: ['babel-loader'],
+    //     include: __dirname,
+    //     exclude: /node_modules/,
+    //   }],
+    // },
+    externals: [nodeExternals()],
+    output: {
+	libraryTarget: 'commonjs',
+	path: path.join(__dirname, '.webpack'),
+	filename: '[name].js',
+    },
 };
